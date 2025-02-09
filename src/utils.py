a407507c798c976496c839e4c1662b15accd515b
farrojo <ofrojasc@gmail.com>
2025-02-09 09:58:54 -0500

    Utils Updated

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,26 @@
-import shutil
+import json
+import os
+
+class LanguageManager:
+    def __init__(self, language='en'):
+        self.language = language
+        self.translations = self.load_language(language)
+
+    def load_language(self, lang):
+        try:
+            with open(f'src/locales/{lang}.json', 'r', encoding='utf-8') as file:
+                return json.load(file)
+        except FileNotFoundError:
+            return {}
+
+    def translate(self, key, **kwargs):
+        text = self.translations.get(key, key)
+        return text.format(**kwargs)
+
+    def set_language(self, lang):
+        self.language = lang
+        self.translations = self.load_language(lang)
 
 def check_pdftk_installed():
-    """
-    Verifica si pdftk está instalado en el sistema.
-    """
+    import shutil
     return shutil.which("pdftk") is not None
